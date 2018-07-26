174ffabf915ea1d64af7822f4201ef13f3a22d30
963559210@qq.com <963559210@qq.com>
2018-07-26 10:34:22 +0800

    jq

---
+++ src/Jq/js/jqjs.js
@@ -0,0 +1,15 @@
+$(function () {
+    $("#btn").click(function () {
+        alert("hello world");
+    });
+});
+
+$(function () {
+    // $("input").click(function () {
+    //     alert("aa");
+    // })
+    // 等选择器
+    // $("p:odd").css("color", "red");
+    // $("p:first").css("color", "red")
+    // $("p:first-child").css("color", "red") 所属父标签的第一个孩子
+});
--- src/Js/js/firstJs.js
+++ src/Js/js/firstJs.js
@@ -16,6 +16,24 @@ function MyClick1() {
     var aNum = [1, 3, 4, 5];
     var oObject = {name:"aaa",age:12}
     var fNumber = 10.23;
-
     var iNum2 = 10, sString = "adsf";
+}
+
+function MyClick2() {
+    var i = 1;
+    var iSum = 0;
+    while (i < 101) {
+        iSum += i
+        i++;
+    }
+    alert(iSum)
+}
+
+function MyClick3() {
+    var sInput = prompt("请输入1-7之间的一个整数");
+    if (sInput == parseInt(sInput))
+    {}
+    else {
+        alert("输入的不是一个整数");
+    }
 }
