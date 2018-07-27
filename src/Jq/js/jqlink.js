be3d340e67b88df9d9fd53871b2943bbbdd3ebab
963559210@qq.com <963559210@qq.com>
2018-07-27 10:58:02 +0800

    jqlink

---
+++ src/Jq/js/jqlink.js
@@ -0,0 +1,7 @@
+$(function () {
+    // $("div").addClass("c1").filter(function (index) {
+    //     return index == 1 || $(this).attr("id") == "d3"
+    // }).addClass("c2");
+    // $("div").find("p").addClass("c2").end().addClass("c1");当前结果的上一个结果集
+    // $("div").find("p").addClass("c2").andSelf().addClass("c1");
+});
